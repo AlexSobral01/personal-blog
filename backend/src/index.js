6380263c122c691380ce8f905f4cb62296c89000
Alex Sobral <alex.dacostasobral@gmail.com>
2023-11-13 15:18:48 -0300

    feat: add database

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,18 @@
 const express = require('express');
 const app = express();
+const articlesController = require('./controllers/articles/ArticlesController');
+
+const db = require('./database/db');
+
+db
+  .authenticate()
+  .then(() => console.log('conexão realizada'))
+  .catch((err) => console.log('ERROR_INTO_DATABASE_CONECTION: ' + err))
+
+app.use('/', articlesController);
+
+app.get('/', (req, res) => {
+  res.send('working');
+})
 
 app.listen(3001, () => console.log('server working port 3001'))
