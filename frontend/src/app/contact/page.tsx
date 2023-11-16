8d256cb19609388ebd0e4e55f3f9ee8279f472ac
Alex Sobral <alex.dacostasobral@gmail.com>
2023-11-16 16:08:47 -0300

    feat: add page contact

---
+++ frontend/src/app/contact/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function ContactPage() {
+  return (
+    <main className='flex flex-col items-center mt-10'>
+      <h1 className='text-2xl font-semibold mb-5'>Pagina de contato</h1>
+      <p>Está pagina está em desnvolvimento, em breve teremos atualizações</p>
+    </main>
+  )
+}
